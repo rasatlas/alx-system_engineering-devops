591e98d7a14d0ef0b04b1cccd19c2b05fdb770e3
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-01-16 18:43:38 +0300

    A function that requests reddit api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A function that queries the Reddit API."""
+import requests 
+import sys
+
+
+if len(sys.argv) > 1:
+    subreddits = sys.argv[1]
+
+def number_of_subscribers(subreddit):
+    """
+    A function that queries the Reddit API and returns
+    the number of subscribers (not active users, total subscribers)
+    for a given subreddit.
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'u/myuseragent'}
+
+    resp = requests.get(url, headers=headers, allow_redirects=False)
+    if resp.status_code == 200:
+        data = resp.json()
+        total_subscribers = data['data']['subscribers']
+        return total_subscribers
+    else:
+        return 0
