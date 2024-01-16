f29659fa776b0b7cf118ca8dd37e6d5796fd2a1e
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-01-16 21:51:52 +0300

    A function that queries reddit api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,8 +9,8 @@ def number_of_subscribers(subreddit):
     the number of subscribers (not active users, total subscribers)
     for a given subreddit.
     """
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': "linux:0x16.api.advanced:v1.0.0 (by /u/guest)"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/guest)"}
 
     resp = requests.get(url, headers=headers, allow_redirects=False)
     if resp.status_code == 200:
