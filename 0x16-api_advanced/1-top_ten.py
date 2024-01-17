adbebec93d6bf72c1920f8e70019aced5b46cedd
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-01-17 13:36:22 +0300

    A function that make request to Reddit API

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""A function that queries the Reddit API."""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    A funciton that queries the Reddit API and prints the titles of
+    the first 10 hot posts listed for a given subreddit.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    params = {"limit": 10}
+    headers = {"User-Agent": "/u/guest"}
+
+    resp = requests.get(url,
+                        headers=headers,
+                        params=params,
+                        allow_redirects=False)
+    if (resp.status_code == 200):
+        data = resp.json()
+        hot_posts = data['data']['children']
+        if len(hot_posts) == 0:
+            print(None)
+        else:
+            for post in hot_posts:
+                print(post['data']['title'])
+                print('\n')
+    else:
+        return 0
