ef79b76fb8f5faedcbf322def7167cc07c715d8b
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-01-23 18:25:52 +0300

    puppet script to fix nginx ulimit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
 # A script to increase the amount of requests an nginx server can handle
 # Increase the ULIMIT of the default nginx file
-exec {'fix_ulimit':
-  command => 'sudo sed -i "s/15/4096" /etc/default/nginx',
+exec { 'fix_ulimit':
+  command => '/usr/bin/sudo /bin/sed -i "s/15/4096/" /etc/default/nginx',
   before  => Exec['restart'],
 }
 
-exec {'restart':
-  command => 'sudo service nginx restart',
+exec { 'restart':
+  command => '/usr/bin/sudo /usr/sbin/service nginx restart',
 }
